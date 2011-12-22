24fd0c00fa5201e87ca3e705134c929445b9bf05
Quan Nguyen <mquannie@gmail.com mailto:mquannie@gmail.com>
2011-12-22 14:16:06 -0800

    add extractor initialize class

--- lib/prizm.rb
+++ lib/prizm.rb
@@ -3,34 +3,31 @@ require 'RMagick'
 
 module Prizm
   class Extractor
-    def get_colors(img_location, nc=6, remove_bg=true)
-      begin
-        image = remove_bg ? remove_bg(img_location, nc) : reduce_size(Magick::ImageList.new(img_location), 100, 100)
+    def initialize(img_location)
+      @image = Magick::ImageList.new(img_location)
+    end
+    
+    def get_colors(nc=6, remove_bg=true)
+        image = remove_bg ? remove_bg(nc) : reduce_size(100, 100)
         q = image.quantize(nc)
         colors = q.color_histogram.sort {|a, b| b[1] <=> a[1]}.map { |color, count| color }
-      rescue Magick::ImageMagickError => e
-        puts e.message
-        puts e.backtrace
-        nil
-      end
     end
     
     def to_hex(pixel)
       pixel.to_color(Magick::AllCompliance, false, 8)
     end
     
-    def reduce_size(image, width, height)
-      image.change_geometry!("#{width}x#{height}") { |cols, rows, img| img.resize!(cols, rows) }
+    def reduce_size(width, height)
+      @image.change_geometry!("#{width}x#{height}") { |cols, rows, img| img.resize!(cols, rows) }
     end
     
-    def remove_bg(img_location, nc, fuzz=20)
-      image = reduce_size Magick::ImageList.new(img_location), 100, 100
+    def remove_bg(nc, fuzz=20)
+      image = reduce_size(100, 100)
       image = image.border(1, 1, image.pixel_color(0,0))
       image = image.quantize(nc+4)
       image.fuzz = "#{fuzz}%"
       a = image.color_floodfill(0, 0, image.pixel_color(0,0))
       a = a.matte_floodfill(0,0)
-      a
     end
   end
 end
--- test/test.rb
+++ test/test.rb
@@ -3,18 +3,17 @@ require 'test/unit'
 
 class TestExtractor < Test::Unit::TestCase
   def test_functional
-    colors = Prizm::Extractor.new.get_colors("#{File.dirname(__FILE__)}/rainbow.jpg", 6, false)
+    colors = Prizm::Extractor.new("#{File.dirname(__FILE__)}/rainbow.jpg").get_colors(6, false)
     assert_equal(6, colors.size)
   end
   
   def test_to_hex
-    extr = Prizm::Extractor.new
-    colors = extr.get_colors("#{File.dirname(__FILE__)}/rainbow.jpg", 6, false)
+    extr = Prizm::Extractor.new("#{File.dirname(__FILE__)}/rainbow.jpg")
+    colors = extr.get_colors(6, false)
     assert_equal('#322D31', extr.to_hex(colors[0]))
   end
   
   def test_error
-    c = Prizm::Extractor.new.get_colors("blahblah.jpg")
-    assert_equal(nil, c)
+    assert_raise(Magick::ImageMagickError) { Prizm::Extractor.new("blahblah.jpg").get_colors }
   end
 end
