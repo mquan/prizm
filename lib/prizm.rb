a0fed4e9cb9c1ca3b501ca39eefb567feae04542
Quan Nguyen <mquannie@gamil.com>
2015-05-24 10:24:20 -0700

    Require rmagick instead of RMagick

--- lib/prizm.rb
+++ lib/prizm.rb
@@ -1,26 +1,28 @@
 require 'rubygems'
-require 'RMagick'
+require 'rmagick'
 
 module Prizm
   class Extractor
     def initialize(img_location)
       @image = Magick::ImageList.new(img_location)
     end
-    
+
     def get_colors(nc=6, remove_bg=true)
         image = remove_bg ? remove_bg(nc) : reduce_size(100, 100)
         q = image.quantize(nc)
         colors = q.color_histogram.sort {|a, b| b[1] <=> a[1]}.map { |color, count| color }
     end
-    
+
     def to_hex(pixel)
       pixel.to_color(Magick::AllCompliance, false, 8)
     end
-    
+
+    private
+
     def reduce_size(width, height)
       @image.change_geometry!("#{width}x#{height}") { |cols, rows, img| img.resize!(cols, rows) }
     end
-    
+
     def remove_bg(nc, fuzz=20)
       image = reduce_size(100, 100)
       image = image.border(1, 1, image.pixel_color(0,0))
--- test/test.rb
+++ test/test.rb
@@ -6,13 +6,13 @@ class TestExtractor < Test::Unit::TestCase
     colors = Prizm::Extractor.new("#{File.dirname(__FILE__)}/rainbow.jpg").get_colors(6, false)
     assert_equal(6, colors.size)
   end
-  
+
   def test_to_hex
     extr = Prizm::Extractor.new("#{File.dirname(__FILE__)}/rainbow.jpg")
     colors = extr.get_colors(6, false)
     assert_equal('#322D31', extr.to_hex(colors[0]))
   end
-  
+
   def test_error
     assert_raise(Magick::ImageMagickError) { Prizm::Extractor.new("blahblah.jpg").get_colors }
   end
